f8a03508fd7de5a05ee92333db2763cd1c36fff2
s-irvine <sam_irvine@hotmail.co.uk>
2016-11-08 14:19:59 +0000

    Update grid.rb

--- Shared/grid.rb
+++ Shared/grid.rb
@@ -1,15 +1,15 @@
 require 'selenium-webdriver'
 require 'rspec/expectations'
 include RSpec::Matchers
-require 'net/http' 
+#require 'net/http' 
 
-http = Net::HTTP.new(@host, @port)
-http.read_timeout = 500
+#http = Net::HTTP.new(@host, @port)
+#http.read_timeout = 500
 
 def setup
   @driver = Selenium::WebDriver.for(
     :remote,
-    #url: 'http://172.19.0.3:5555',
+    url: 'http://172.19.0.3:5555',
     desired_capabilities: :firefox) # you can also use :chrome, :safari, etc.
 end
 
@@ -24,7 +24,7 @@ def run
 end
 
 run do
-  @driver.manage.timeouts.page_load = 300
+  #@driver.manage.timeouts.page_load = 300
   @driver.get 'http://the-internet.herokuapp.com'
   expect(@driver.title).to eq('The Internet')
 end
