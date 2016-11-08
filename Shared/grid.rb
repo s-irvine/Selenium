1915fdceb9e2f0b7a50a31417356ee58b270174b
s-irvine <sam_irvine@hotmail.co.uk>
2016-11-08 10:41:42 +0000

    Update grid.rb

--- Shared/grid.rb
+++ Shared/grid.rb
@@ -5,7 +5,7 @@ include RSpec::Matchers
 def setup
   @driver = Selenium::WebDriver.for(
     :remote,
-    url: 'http://172.19.0.3:5555',
+    #url: 'http://172.19.0.3:5555',
     desired_capabilities: :firefox) # you can also use :chrome, :safari, etc.
 end
 
