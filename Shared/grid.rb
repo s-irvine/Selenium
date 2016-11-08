6d012b16d0c97253124f53188cf83f9ba322a8bc
s-irvine <sam_irvine@hotmail.co.uk>
2016-11-08 14:34:59 +0000

    Update grid.rb

--- Shared/grid.rb
+++ Shared/grid.rb
@@ -1,49 +1,26 @@
-#require 'selenium-webdriver'
-#require 'rspec/expectations'
-#include RSpec::Matchers
-##require 'net/http' 
-
-##http = Net::HTTP.new(@host, @port)
-##http.read_timeout = 500
-
-#def setup
-#  @driver = Selenium::WebDriver.for(
-#    :remote,
-#    url: 'http://172.19.0.3:5555/wd/hub',
-#    #url: any,
-#    desired_capabilities: :firefox) # you can also use :chrome, :safari, etc.
-#end
-
-#def teardown
-#  @driver.quit
-#end
-
-#def run
-#  setup
-#  yield
-#  teardown
-#end
-
-#run do
-#  #@driver.manage.timeouts.page_load = 300
-#  @driver.get 'http://the-internet.herokuapp.com'
-#  expect(@driver.title).to eq('The Internet')
-#end
-
-require 'rubygems'
 require 'selenium-webdriver'
+require 'rspec/expectations'
+include RSpec::Matchers
+#require 'net/http' 
 
-driver = Selenium::WebDriver.for :firefox
-driver.get "http://google.com"
+#http = Net::HTTP.new(@host, @port)
+#http.read_timeout = 500
 
-element = driver.find_element :name => "q"
-element.send_keys "Cheese!"
-element.submit
+def setup
+  @driver = Selenium::WebDriver.for(
+    :remote,
+    url: 'http://172.19.0.3:5555/wd/hub',
+    #url: any,
+    desired_capabilities: :firefox) # you can also use :chrome, :safari, etc.
+end
 
-puts "Page title is #{driver.title}"
+def teardown
+  @driver.quit
+end
 
-wait = Selenium::WebDriver::Wait.new(:timeout => 10)
-wait.until { driver.title.downcase.start_with? "cheese!" }
+def run
+  setup
+  yield
+  teardown
+end
 
-puts "Page title is #{driver.title}"
-driver.quit
