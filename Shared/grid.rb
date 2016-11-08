6b7e9ec903f297851e092b5fd4a42860d7eaa707
s-irvine <sam_irvine@hotmail.co.uk>
2016-11-08 09:28:34 +0000

    Example Test

---
+++ Shared/grid.rb
@@ -0,0 +1,27 @@
+
+
+require 'selenium-webdriver'
+require 'rspec/expectations'
+include RSpec::Matchers
+
+def setup
+  @driver = Selenium::WebDriver.for(
+    :remote,
+    url: 'http://selnode1:4444/wd/hub',
+    desired_capabilities: :firefox) # you can also use :chrome, :safari, etc.
+end
+
+def teardown
+  @driver.quit
+end
+
+def run
+  setup
+  yield
+  teardown
+end
+
+run do
+  @driver.get 'http://the-internet.herokuapp.com'
+  expect(@driver.title).to eq('The Internet')
+end
