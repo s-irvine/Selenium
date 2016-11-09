18d8ed74cbef34544c48d78aa206ffb4d247063e
s-irvine <sam_irvine@hotmail.co.uk>
2016-11-09 15:11:56 +0000

    Update git.rb

--- git.rb
+++ git.rb
@@ -7,7 +7,7 @@ driver.get "https://github.com/s-irvine/Selenium"
 
 puts "Page title is #{driver.title}"
 
-element = driver.find_element :Xpath => "//*[@id="js-repo-pjax-container"]/div[2]/div[1]/div[5]/div[2]/form/button"   
+element = driver.find_element :xpath => "//*[@id="js-repo-pjax-container"]/div[2]/div[1]/div[5]/div[2]/form/button"   
 element.submit
 
 wait = Selenium::WebDriver::Wait.new(:timeout => 10)
@@ -15,10 +15,10 @@ wait.until { driver.title.downcase.start_with? "new" }
 
 puts "Page title is #{driver.title}"
 
-element = driver.find_element :Xpath => "//*[@id="js-repo-pjax-container"]/div[2]/div[1]/div/form[2]/div[2]/input[1]"  
+element = driver.find_element :xpath => "//*[@id="js-repo-pjax-container"]/div[2]/div[1]/div/form[2]/div[2]/input[1]"  
 element.send_keys "test" 
 
-element = driver.find_element :Xpath => "//*[@id="submit-file"]"
+element = driver.find_element :xpath => "//*[@id="submit-file"]"
 element.submit
   
 wait = Selenium::WebDriver::Wait.new(:timeout => 10)
