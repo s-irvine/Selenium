20d0e5dfae56a68f36ae8bc3bd84bd37dc8d8dc6
s-irvine <sam_irvine@hotmail.co.uk>
2016-11-09 15:00:00 +0000

    Adding git.rb

---
+++ git.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'selenium-webdriver'
+
+driver = Selenium::WebDriver.for(:remote,url: 'http://52.213.84.182:4444/wd/hub',desired_capabilities: :firefox)
+
+for i in 0..10
+   puts "Iteration number #{i}"
+   
+   driver.get "http://google.com"
+     
+   element = driver.find_element :name => "q"
+   element.send_keys "Number #{i}" 
+   element.submit
+
+   puts "Page title is #{driver.title}"
+  
+   wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+   wait.until { driver.title.downcase.start_with? "num" }
+
+   puts "Page title is #{driver.title}"
+end
+driver.quit
