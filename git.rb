352f951db13589a9c5b497cc2c6f9d5c877f9851
s-irvine <sam_irvine@hotmail.co.uk>
2016-11-09 17:26:55 +0000

    Update git.rb

--- git.rb
+++ git.rb
@@ -9,7 +9,7 @@ driver.get "https://github.com/s-irvine/Selenium"
 puts "Page title is #{driver.title}"
 
 #element = driver.find_element(:xpath, '//*[@id="js-repo-pjax-container"]/div[2]/div[1]/div[5]/div[2]/form/button')
-driver.switch_to.frame "form"
+driver.switch_to.frame "js-repo-pjax-container"
 element = driver.find_elements(:class, 'btn btn-sm BtnGroup-item')
 element[0].click
 #driver.find_element(:id, "Create new file").click 
