c304895199672b8756436699dcb74cb4ae09d498
s-irvine <sam_irvine@hotmail.co.uk>
2016-11-09 11:11:38 +0000

    Update grid.rb

--- Shared/grid.rb
+++ Shared/grid.rb
@@ -1,15 +1,5 @@
 require 'rubygems'
 require 'selenium-webdriver'
-require 'rspec/retry'
-
-RSpec.configure do |config|
-  # show retry status in spec process
-  #config.verbose_retry = true
-  # Try twice (retry once)
-  config.default_retry_count = 5
-  # Only retry when Selenium raises Net::ReadTimeout
-  config.exceptions_to_retry = [Net::ReadTimeout]
-end
 
 driver = Selenium::WebDriver.for(:remote,desired_capabilities: :firefox)
 #, url: 'http://172.19.0.3:5555/wd/hub'
