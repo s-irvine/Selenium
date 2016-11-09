400795d0a3dbfd36407cca7c7ec07df7a69c3d2f
s-irvine <sam_irvine@hotmail.co.uk>
2016-11-09 14:00:49 +0000

    Update grid.rb

--- grid.rb
+++ grid.rb
@@ -5,13 +5,13 @@ driver = Selenium::WebDriver.for(:remote,url: 'http://52.213.84.182:4444/wd/hub'
 driver.get "http://google.com"
 
 element = driver.find_element :name => "q"
-element.send_keys "Cheese!"
+element.send_keys "TRUMP 2016!!"
 element.submit
 
 puts "Page title is #{driver.title}"
 
 wait = Selenium::WebDriver::Wait.new(:timeout => 10)
-wait.until { driver.title.downcase.start_with? "cheese!" }
+wait.until { driver.title.downcase.start_with? "trump" }
 
 puts "Page title is #{driver.title}"
 driver.quit
