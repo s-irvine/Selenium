2ca41833c60ee224a0fd9c8768593781bd5bdc7c
s-irvine <sam_irvine@hotmail.co.uk>
2016-11-09 15:10:19 +0000

    Update git.rb

--- git.rb
+++ git.rb
@@ -3,20 +3,27 @@ require 'selenium-webdriver'
 
 driver = Selenium::WebDriver.for(:remote,url: 'http://52.213.84.182:4444/wd/hub',desired_capabilities: :firefox)
 
-for i in 0..10
-   puts "Iteration number #{i}"
-   
-   driver.get "http://google.com"
-     
-   element = driver.find_element :name => "q"
-   element.send_keys "Number #{i}" 
-   element.submit
+driver.get "https://github.com/s-irvine/Selenium"
 
-   puts "Page title is #{driver.title}"
+puts "Page title is #{driver.title}"
+
+element = driver.find_element :Xpath => "//*[@id="js-repo-pjax-container"]/div[2]/div[1]/div[5]/div[2]/form/button"   
+element.submit
+
+wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+wait.until { driver.title.downcase.start_with? "new" }
+
+puts "Page title is #{driver.title}"
+
+element = driver.find_element :Xpath => "//*[@id="js-repo-pjax-container"]/div[2]/div[1]/div/form[2]/div[2]/input[1]"  
+element.send_keys "test" 
+
+element = driver.find_element :Xpath => "//*[@id="submit-file"]"
+element.submit
   
-   wait = Selenium::WebDriver::Wait.new(:timeout => 10)
-   wait.until { driver.title.downcase.start_with? "num" }
+wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+wait.until { driver.title.downcase.start_with? "s-" }
 
-   puts "Page title is #{driver.title}"
-end
+puts "Page title is #{driver.title}"
+
 driver.quit
