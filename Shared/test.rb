a0e1c8ff441bc58a10d41161d21f51cf0b1fcb3f
s-irvine <sam_irvine@hotmail.co.uk>
2016-11-09 13:47:45 +0000

    Test rb file

---
+++ Shared/test.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+require 'selenium-webdriver'
+
+driver = Selenium::WebDriver.for(:remote,url: 'http://52.213.84.182:4444/wd/hub',desired_capabilities: :firefox)
+driver.get "http://google.com"
+
+element = driver.find_element :name => "q"
+element.send_keys "Cheese!"
+element.submit
+
+puts "Page title is #{driver.title}"
+
+wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+wait.until { driver.title.downcase.start_with? "cheese!" }
+
+puts "Page title is #{driver.title}"
+driver.quit
