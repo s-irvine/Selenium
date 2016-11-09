cb914fffe13aa176b15648015f7250ca4848ebd4
s-irvine <sam_irvine@hotmail.co.uk>
2016-11-09 15:26:25 +0000

    Update git.rb

--- git.rb
+++ git.rb
@@ -7,7 +7,8 @@ driver.get "https://github.com/s-irvine/Selenium"
 
 puts "Page title is #{driver.title}"
 
-element = driver.find_element(:xpath, "//*[@id='js-repo-pjax-container']/div[2]/div[1]/div[5]/div[2]/form/button").click
+#driver.find_element(:xpath, "//*[@id='js-repo-pjax-container']/div[2]/div[1]/div[5]/div[2]/form/button").click
+driver.find_element(:css, "Create new file").click
 
 wait = Selenium::WebDriver::Wait.new(:timeout => 10)
 wait.until { driver.title.downcase.start_with? "new" }
@@ -17,7 +18,7 @@ puts "Page title is #{driver.title}"
 element = driver.find_element(:xpath, "//*[@id='js-repo-pjax-container']/div[2]/div[1]/div/form[2]/div[2]/input[1]") 
 element.send_keys "test" 
 
-element = driver.find_element(:xpath, "//*[@id='submit-file']").click
+driver.find_element(:xpath, "//*[@id='submit-file']").click
   
 wait = Selenium::WebDriver::Wait.new(:timeout => 10)
 wait.until { driver.title.downcase.start_with? "s-" }
