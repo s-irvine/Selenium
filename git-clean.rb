f543b950cd60b3057f0a6c7c517b1aa6a39e8ac6
s-irvine <sam_irvine@hotmail.co.uk>
2016-11-10 10:59:08 +0000

    Update git-clean.rb

--- git-clean.rb
+++ git-clean.rb
@@ -33,7 +33,7 @@ for i in 6..9
   driver.find_element(:xpath, "//*[@title='Test - #{i}']").click
   
   wait = Selenium::WebDriver::Wait.new(:timeout => 15)
-  wait.until { driver.title.start_with? "Selenium/Test" }
+  wait.until {driver.title "Selenium/Test - #{i} at master · s-irvine/Selenium"}
   
   puts "Page title is #{driver.title}"
   
