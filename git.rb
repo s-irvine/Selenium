ef5c0916b2b0132330f6927af8081f87476780c5
s-irvine <sam_irvine@hotmail.co.uk>
2016-11-10 14:19:50 +0000

    Update git.rb

--- git.rb
+++ git.rb
@@ -8,70 +8,48 @@ start = Time.now
 user = "s-irvine"
 pass = "Ripcurl12"
 
-for i in 1..3
-  puts "Iteration number #{i}"
-  
-  driver = Selenium::WebDriver.for(:remote,url: 'http://52.48.17.19:4444//wd/hub',desired_capabilities: :firefox)
-  driver.manage.timeouts.implicit_wait = 10
+driver = Selenium::WebDriver.for(:remote,url: 'http://52.48.17.19:4444//wd/hub',desired_capabilities: :firefox)
+driver.manage.timeouts.implicit_wait = 10
 
-  driver.get "https://github.com/s-irvine/Selenium"
+driver.get "https://github.com/s-irvine/Selenium"
 
-  puts "Page title is #{driver.title}"
+puts "Page title is #{driver.title}"
 
-  driver.find_element(:xpath, "/html/body/header/div/div/div/a[2]").click
+driver.find_element(:xpath, "/html/body/header/div/div/div/a[2]").click
 
-  wait = Selenium::WebDriver::Wait.new(:timeout => 10)
-  wait.until { driver.title.downcase.start_with? "sign" }
+wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+wait.until { driver.title.downcase.start_with? "sign" }
   
-  #sleep(10)
+puts "Page title is #{driver.title}"
+  
+driver.find_element(:id, "login_field").send_keys "#{user}"
+driver.find_element(:id, "password").send_keys "#{pass}"
+driver.find_element(:xpath, '//*[@id="login"]/form/div[4]/input[3]').click  
 
-  puts "Page title is #{driver.title}"
+wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+wait.until { driver.title.downcase.start_with? "s-irvine" }
   
-  driver.find_element(:id, "login_field").send_keys "#{user}"
-  driver.find_element(:id, "password").send_keys "#{pass}"
-  driver.find_element(:xpath, '//*[@id="login"]/form/div[4]/input[3]').click  
+puts "Page title is #{driver.title}"
+  
+driver.find_element(:xpath, '//*[@id="js-repo-pjax-container"]/div[2]/div[1]/div[5]/div[2]/form/button').click
 
-  wait = Selenium::WebDriver::Wait.new(:timeout => 10)
-  wait.until { driver.title.downcase.start_with? "s-irvine" }
+wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+wait.until { driver.title.downcase.start_with? "new" }
   
-  #sleep(10)
+puts "Page title is #{driver.title}"
 
-  puts "Page title is #{driver.title}"
+element = driver.find_element(:xpath, "//*[@id='js-repo-pjax-container']/div[2]/div[1]/div/form[2]/div[2]/input[1]") 
+element.send_keys "Test - #{i}" 
+element.submit
   
-  driver.find_element(:xpath, '//*[@id="js-repo-pjax-container"]/div[2]/div[1]/div[5]/div[2]/form/button').click
+driver.find_element(:id, "submit-file").click
 
-  wait = Selenium::WebDriver::Wait.new(:timeout => 10)
-  wait.until { driver.title.downcase.start_with? "new" }
+wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+wait.until { driver.title.downcase.start_with? "s-irvine" }
   
-  #sleep(10)
+puts "Page title is #{driver.title}"
 
-  puts "Page title is #{driver.title}"
-
-  element = driver.find_element(:xpath, "//*[@id='js-repo-pjax-container']/div[2]/div[1]/div/form[2]/div[2]/input[1]") 
-  element.send_keys "Test - #{i}" 
-  element.submit
-  
-  #m=0
-  
-  #until  driver.title.downcase.start_with? "s-irvine" 
-    
-    driver.find_element(:id, "submit-file").click
-    #sleep(0.5)
-    #m=m+1
-    
-  #end
-  
-  #puts "Needed to try #{m} times(s) to commit the new file"
-  
-  wait = Selenium::WebDriver::Wait.new(:timeout => 10)
-  wait.until { driver.title.downcase.start_with? "s-irvine" }
-  
-  #sleep(10)
-
-  puts "Page title is #{driver.title}"
-  
-  driver.quit
-end
+driver.quit
 
 fin = Time.now
 run = fin-start
