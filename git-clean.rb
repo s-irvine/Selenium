d8149e33fd8bb548fc6d3fec9a65e516c4939617
s-irvine <sam_irvine@hotmail.co.uk>
2016-11-10 11:22:28 +0000

    Update git-clean.rb

--- git-clean.rb
+++ git-clean.rb
@@ -14,41 +14,51 @@ for i in 1..3
 
   driver.find_element(:xpath, "/html/body/header/div/div/div/a[2]").click
 
-  wait = Selenium::WebDriver::Wait.new(:timeout => 10)
-  wait.until { driver.title.downcase.start_with? "sign" }
+  #wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+  #wait.until { driver.title.downcase.start_with? "sign" }
 
+  sleep(10)
+  
   puts "Page title is #{driver.title}"
 
   driver.find_element(:id, "login_field").send_keys "#{user}"
   driver.find_element(:id, "password").send_keys "#{pass}"
   driver.find_element(:xpath, '//*[@id="login"]/form/div[4]/input[3]').click  
 
-  wait = Selenium::WebDriver::Wait.new(:timeout => 10)
-  wait.until { driver.title.downcase.start_with? "s-irvine" }
+  #wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+  #wait.until { driver.title.downcase.start_with? "s-irvine" }
 
+  sleep(10)
+  
   puts "Page title is #{driver.title}"
 
   puts "Iteration number #{i}"
   
   driver.find_element(:xpath, "//*[@title='Test - #{i}']").click
   
-  wait = Selenium::WebDriver::Wait.new(:timeout => 15)
-  wait.until {driver.title.start_with? "Selenium/Test - #{i} at master"}
+  #wait = Selenium::WebDriver::Wait.new(:timeout => 15)
+  #wait.until {driver.title.start_with? "Selenium/Test - #{i} at master"}
+  
+  sleep(10)
   
   puts "Page title is #{driver.title}"
   
   driver.find_element(:xpath, '//*[@id="js-repo-pjax-container"]/div[2]/div[1]/div[3]/div[1]/div[1]/form[2]/button').click
   
-  wait = Selenium::WebDriver::Wait.new(:timeout => 10)
-  wait.until { driver.title.downcase.start_with? "deleting" }
+  #wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+  #wait.until { driver.title.downcase.start_with? "deleting" }
+  
+  sleep(10)
   
   puts "Page title is #{driver.title}"
   
   driver.find_element(:id, "submit-file").click
   
-  wait = Selenium::WebDriver::Wait.new(:timeout => 10)
-  wait.until { driver.title.downcase.start_with? "s-irvine" }
+  #wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+  #wait.until { driver.title.downcase.start_with? "s-irvine" }
 
+  sleep(10)
+  
   puts "Page title is #{driver.title}"
   
   driver.quit
