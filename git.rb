c4e6756d7c676b01a1c91f3e14a669af3f522006
s-irvine <sam_irvine@hotmail.co.uk>
2016-11-10 10:16:21 +0000

    Update git.rb

--- git.rb
+++ git.rb
@@ -4,32 +4,32 @@ require 'selenium-webdriver'
 user = "s-irvine"
 pass = "Ripcurl12"
 
+driver = Selenium::WebDriver.for(:remote,url: 'http://52.48.17.19:4444//wd/hub',desired_capabilities: :firefox)
+driver.manage.timeouts.implicit_wait = 5
+
+driver.get "https://github.com/s-irvine/Selenium"
+
+puts "Page title is #{driver.title}"
+
+driver.find_element(:xpath, "/html/body/header/div/div/div/a[2]").click
+
+wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+wait.until { driver.title.downcase.start_with? "sign" }
+
+puts "Page title is #{driver.title}"
+
+driver.find_element(:id, "login_field").send_keys "#{user}"
+driver.find_element(:id, "password").send_keys "#{pass}"
+driver.find_element(:xpath, '//*[@id="login"]/form/div[4]/input[3]').click  
+
+wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+wait.until { driver.title.downcase.start_with? "s-irvine" }
+
+puts "Page title is #{driver.title}"
+
 for i in 1..10
   
   puts "Iteration number: #{i}"
-
-  driver = Selenium::WebDriver.for(:remote,url: 'http://52.48.17.19:4444//wd/hub',desired_capabilities: :firefox)
-  driver.manage.timeouts.implicit_wait = 5
-
-  driver.get "https://github.com/s-irvine/Selenium"
-
-  puts "Page title is #{driver.title}"
-
-  driver.find_element(:xpath, "/html/body/header/div/div/div/a[2]").click
-
-  wait = Selenium::WebDriver::Wait.new(:timeout => 10)
-  wait.until { driver.title.downcase.start_with? "sign" }
-
-  puts "Page title is #{driver.title}"
-
-  driver.find_element(:id, "login_field").send_keys "#{user}"
-  driver.find_element(:id, "password").send_keys "#{pass}"
-  driver.find_element(:xpath, '//*[@id="login"]/form/div[4]/input[3]').click  
-
-  wait = Selenium::WebDriver::Wait.new(:timeout => 10)
-  wait.until { driver.title.downcase.start_with? "s-irvine" }
-
-  puts "Page title is #{driver.title}"
 
   driver.find_element(:xpath, '//*[@id="js-repo-pjax-container"]/div[2]/div[1]/div[5]/div[2]/form/button').click
 
@@ -47,8 +47,9 @@ for i in 1..10
   wait.until { driver.title.downcase.start_with? "s-irvine" }
 
   puts "Page title is #{driver.title}"
-
-  driver.quit
+  
 end
+
+driver.quit
 
 puts "Test has completed!" 
